8e54309475232cdcbf75d79cfeb8b76d2a82e890
110062124 <liuericboy@gmail.com>
2024-07-01 21:31:51 +0800

    second commit

--- my-opensearch-function/opensearch-lambda.py
+++ my-opensearch-function/opensearch-lambda.py
@@ -9,10 +9,12 @@ credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
 host = 'https://search-linebot-search-3uafa4oepdfjx4tfk3emvsh5a4.aos.ap-northeast-3.on.aws' # The OpenSearch domain endpoint with https:// and without a trailing slash
+datatype = '_doc'
 # index = 'movies'
-index = 'products-shirts'
-url = host + '/' + index + '/_search'
-
+index = 'test-index'
+# url = host + '/' + index + '/_search'
+# url_index = host + '/' + index
+# url_post = host + '/' + index + '/' + datatype + '/'
 # Lambda execution starts here
 def lambda_handler(event, context):
 
@@ -22,59 +24,111 @@ def lambda_handler(event, context):
     #     "size": 25,
     #     "query": {
     #         "multi_match": {
-    #             "query": event['queryStringParameters']['q'],
+    #             "query": "thor",
     #             "fields": ["title^4", "plot^2", "actors", "directors"]
     #         }
     #     }
     # }
+  #   query = {
+  #   "size": 2,
+  #   "query": {
+  #     "knn": {
+  #       "item_vector": {
+  #         "vector": [
+  #           2, 4, 3
+  #         ],
+  #         "k": 10,
+  #         "filter": {
+  #           "bool": {
+  #             "must": [
+  #               {
+  #                 "range": {
+  #                   "rating": {
+  #                     "gte": 1,
+  #                     "lte": 10
+  #                   }
+  #                 }
+  #               }
+  #             ]
+  #           }
+  #         }
+  #       }
+  #     }
+  #   }
+  # }
     query = {
-    "size": 2,
-    "query": {
-      "knn": {
-        "item_vector": {
-          "vector": [
-            2, 4, 3
-          ],
-          "k": 10,
-          "filter": {
-            "bool": {
-              "must": [
-                {
-                  "range": {
-                    "rating": {
-                      "gte": 7,
-                      "lte": 10
-                    }
-                  }
-                },
-                {
-                  "term": {
-                    "size": "small"
-                  }
-                }
-              ]
-            }
+      "size": 2,
+      "query": {
+        "knn": {
+          "embedding": {
+            "vector": [
+              2, 4, 3
+            ],
+            "k": 10
           }
         }
       }
     }
-  }
+    
+    # test_index = '{"settings": {"index": {"knn": true}},"mappings": {"properties": {"item_vector": {"type": "knn_vector","dimension": 3,"method": {"name": "hnsw","space_type": "l2","engine": "faiss"}}}}}'
+    test_index = """{
+    "settings": {
+       "index.knn": true
+    },
+    "mappings": {
+       "properties": {
+          "embedding": {
+             "type": "knn_vector",
+             "dimension": 3
+          },
+          "message": {
+             "type": "text"
+          }
+       }
+    }
+}"""
+    
+    data = json.loads(test_index)
+    # post_item = '{ "item_vector": [6.4, 3.4, 6.6], "size" : "small", "rating" : 9 }'
+    # print(post_item)
+    # # data_post = json.loads(post_item)
+    # post_item = '{"embedding": ' + str(event['queryStringParameters']['embedding']) + ', "message": "' + event['queryStringParameters']['message'] + '"}'
+    # print(post_item)
+    # data_post = json.loads(post_item)
 
+
+    index = 'test-index'
+    index = event['queryStringParameters']['q']
+    url = host + '/' + index + '/_search'
+    url_index = host + '/' + index
+    url_post = host + '/' + index + '/' + datatype + '/'
+    
     # Elasticsearch 6.x requires an explicit Content-Type header
     headers = { "Content-Type": "application/json" }
-
+    if(event['queryStringParameters']['type'] == "Get"):
+        r = requests.get(url, auth=awsauth, headers=headers, data=json.dumps(query))
+    elif(event['queryStringParameters']['type'] == "Post"):
+        r = requests.post(url_post + event['queryStringParameters']['id'], auth=awsauth, headers=headers, data=json.dumps(data_post))
+    elif(event['queryStringParameters']['type'] == "Put"):
+        r = requests.put(url_index, auth=awsauth, headers=headers, data=json.dumps(data))
+    else:
+        print("No Re", type(event['queryStringParameters']['type']), event['queryStringParameters']['type'])
+    
     # Make the signed HTTP request
-    r = requests.get(url, auth=awsauth, headers=headers, data=json.dumps(query))
+    # r = requests.get(url, auth=awsauth, headers=headers, data=json.dumps(query))
+    # r = requests.put(url_index, auth=awsauth, headers=headers, data=json.dumps(data))
+    # r = requests.post(url_post + '1', auth=awsauth, headers=headers, data=json.dumps(data))
 
     # Create the response and add some extra content to support CORS
     response = {
         "statusCode": 200,
         "headers": {
-            "Access-Control-Allow-Origin": '*'
+            "Access-Control-Allow-Origin": '*',
         },
         "isBase64Encoded": False
     }
 
     # Add the search results to the response
     response['body'] = r.text
+    print(event['queryStringParameters'])
     return response
--- sample-site/scripts/search.js
+++ sample-site/scripts/search.js
@@ -1,54 +1,89 @@
 // Update this variable to point to your domain.
-var apigatewayendpoint = 'https://i4xobr4do3.execute-api.ap-northeast-3.amazonaws.com/opensearch-api-test/';
+var apigatewayendpoint = 'https://o0p2k383h8.execute-api.ap-northeast-3.amazonaws.com/opensearch-api-test/';
 var loadingdiv = $('#loading');
 var noresults = $('#noresults');
 var resultdiv = $('#results');
 var searchbox = $('input#search');
+var searchbox2 = $('input#search2');
+var searchbox3 = $('input#search3');
+var searchbox4 = $('input#search4');
 var timer = 0;
 
 // Executes the search function 250 milliseconds after user stops typing
-searchbox.keyup(function () {
-  clearTimeout(timer);
-  timer = setTimeout(search, 250);
-});
+// searchbox.keyup(function () {
+//   clearTimeout(timer);
+//   timer = setTimeout(search, 250);
+// });
 
-async function search() {
-  // Clear results before searching
-  noresults.hide();
-  resultdiv.empty();
-  loadingdiv.show();
-  // Get the query from the user
-  let query = searchbox.val();
-  // Only run a query if the string contains at least three characters
-  if (query.length > 2) {
-    // Make the HTTP request with the query as a parameter and wait for the JSON results
-    let response = await $.get(apigatewayendpoint, { q: query, size: 25 }, 'json');
-    // Get the part of the JSON response that we care about
-    console.log(response['hits']['hits'])
-    let results = response['hits']['hits'];
-    if (results.length > 0) {
-      loadingdiv.hide();
-      // Iterate through the results and write them to HTML
-      resultdiv.append('<p>Found ' + results.length + ' results.</p>');
-      for (var item in results) {
-        let url = 'https://www.imdb.com/title/' + results[item]._id;
-        let image = results[item]._source.image_url;
-        let title = results[item]._source.title;
-        let plot = results[item]._source.plot;
-        let year = results[item]._source.year;
-        // Construct the full HTML string that we want to append to the div
-        resultdiv.append('<div class="result">' +
-        '<a href="' + url + '"><img src="' + image + '" onerror="imageError(this)"></a>' +
-        '<div><h2><a href="' + url + '">' + title + '</a></h2><p>' + year + ' &mdash; ' + plot + '</p></div></div>');
-      }
-    } else {
-      noresults.show();
-    }
-  }
-  loadingdiv.hide();
-}
+// async function search() {
+//   // Clear results before searching
+//   noresults.hide();
+//   resultdiv.empty();
+//   loadingdiv.show();
+//   // Get the query from the user
+//   let query = searchbox.val();
+//   // Only run a query if the string contains at least three characters
+//   if (query.length > 2) {
+//     // Make the HTTP request with the query as a parameter and wait for the JSON results
+//     let response = await $.get(apigatewayendpoint, { q: query, size: 25 }, 'json');
+//     // Get the part of the JSON response that we care about
+//     console.log(response['hits']['hits'])
+//     let results = response['hits']['hits'];
+//     if (results.length > 0) {
+//       loadingdiv.hide();
+//       // Iterate through the results and write them to HTML
+//       resultdiv.append('<p>Found ' + results.length + ' results.</p>');
+//       for (var item in results) {
+//         let url = 'https://www.imdb.com/title/' + results[item]._id;
+//         let image = results[item]._source.image_url;
+//         let title = results[item]._source.title;
+//         let plot = results[item]._source.plot;
+//         let year = results[item]._source.year;
+//         // Construct the full HTML string that we want to append to the div
+//         resultdiv.append('<div class="result">' +
+//         '<a href="' + url + '"><img src="' + image + '" onerror="imageError(this)"></a>' +
+//         '<div><h2><a href="' + url + '">' + title + '</a></h2><p>' + year + ' &mdash; ' + plot + '</p></div></div>');
+//       }
+//     } else {
+//       noresults.show();
+//     }
+//   } else {
+//     await $.get(apigatewayendpoint, { q: query, size: 25 }, 'json');
+//   }
+//   loadingdiv.hide();
+// }
 
 // Tiny function to catch images that fail to load and replace them
 function imageError(image) {
   image.src = 'images/no-image.png';
+}
+
+async function myGet() {
+    let index = searchbox.val();
+    let _message = searchbox2.val();
+    let _embedding = searchbox3.val();
+    let _id = searchbox4.val();
+    let response = await $.get(apigatewayendpoint, { type: "Get", size: 25 , q: index, message: _message, embedding: _embedding, id: _id}, 'json');
+    // Get the part of the JSON response that we care about
+    console.log(response)
+}
+
+async function myPost() {
+  let index = searchbox.val();
+    let _message = searchbox2.val();
+    let _embedding = searchbox3.val();
+    let _id = searchbox4.val();
+    let response = await $.get(apigatewayendpoint, { type: "Post", size: 25 , q: index, message: _message, embedding: _embedding, id: _id}, 'json');
+  // Get the part of the JSON response that we care about
+  console.log(response)
+}
+
+async function myPut() {
+  let index = searchbox.val();
+    let _message = searchbox2.val();
+    let _embedding = searchbox3.val();
+    let _id = searchbox4.val();
+    let response = await $.get(apigatewayendpoint, { type: "Put", size: 25 , q: index, message: _message, embedding: _embedding, id: _id}, 'json');
+  // Get the part of the JSON response that we care about
+  console.log(response)
 }
